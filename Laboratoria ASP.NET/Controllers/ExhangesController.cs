d49e9e434caba59fce2539e79c7c04851664eddf
Dariusz Kubacki <dariusz.kubacki@microsoft.wsei.edu.pl>
2020-10-11 17:57:41 +0200

    Lab1

---
+++ Laboratoria ASP.NET/Controllers/ExhangesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratoria_ASP.NET.Controllers
+{
+    public class ExhangesController : Controller
+    {
+        public IActionResult ShowView ()
+        {
+            return View();
+        }
+
+    }
+}
