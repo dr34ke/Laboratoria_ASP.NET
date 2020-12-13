a908ef6a504d82486e13056adba8d3b63e77d6c3
Dariusz Kubacki <dariusz.kubacki@microsoft.wsei.edu.pl>
2020-12-13 13:41:33 +0100

    lab4

---
+++ Laboratoria ASP.NET/ViewComponents/LatestItem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laboratoria_ASP.NET.Database;
+
+namespace Laboratoria_ASP.NET.ViewComponents
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext dbContext;
+
+        public LatestItem(ExchangesDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = dbContext.Items.OrderByDescending(x => x.Id).First();
+            return View("Index", latestItem);
+        }
+
+    }
+}
